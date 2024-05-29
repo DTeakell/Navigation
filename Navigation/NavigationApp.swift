70a27d5697e1207b67e09e7b123b87430b104c77
Dillon Teakell <teakelld@icloud.com>
2024-05-29 13:18:25 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Dillon Teakell on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
